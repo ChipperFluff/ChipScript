6c97fe195580bc52c3b55ff8f619533f199572ad
Dominik Krenn <dominik.krenn@1tool.com>
2025-02-28 12:45:31 +0100

    brought back squirrel mascot

--- system.sh
+++ system.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-alias chipi.version="echo ChipScript 1.1.0 \U0001F43F️"
+alias chipi.version="echo ChipScript 1.1.0 🐿️"
 alias chipi.reload="source ~/.bashrc"
 alias chipi.list="ls -a $SCRIPT_PATH/scripts"
 alias chipi.git="git -C \"$SCRIPT_PATH\""
